12010edcc00e2266cd3c7a0c55e153308435f096
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-01-26 13:15:39 +0200

    tehty testejä korteille

---
+++ pasianssi/src/test/java/fi/jussi/pasianssi/kortit/KorttiTest.java
@@ -0,0 +1,46 @@
+package fi.jussi.pasianssi.kortit;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class KorttiTest {
+    Kortti kortti;
+    
+    public KorttiTest() {
+        
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        kortti = new Kortti(Maa.HERTTA, 5);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void kortillaMaa() {
+        assertEquals(kortti.getMaa(), Maa.HERTTA);
+    }
+    
+    @Test
+    public void kortillaArvo() {
+        assertEquals(kortti.getArvo(), 5);
+    }
+    
+    
+}
